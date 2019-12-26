80bc15c40f0876260495b9343e78cafe29602dfd
Tin <tinaungkhine@gmail.com>
2019-12-26 17:45:24 -0500

    Add files via upload

---
+++ precache-manifest.a4db360a23fa21853060146db82bad27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e63c1079bf4798228fb75d0be24d77c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f1dd015db40e1fe284f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "36dce862b55ac93c185b",
+    "url": "/static/js/2.fa28abaf.chunk.js"
+  },
+  {
+    "revision": "6f1dd015db40e1fe284f",
+    "url": "/static/js/main.2a6db15b.chunk.js"
+  },
+  {
+    "revision": "d40bdb0faa80fc28c9c2",
+    "url": "/static/js/runtime-main.8ea19656.js"
+  }
+]);
