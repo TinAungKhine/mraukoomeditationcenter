71034b9410447fecc4a8d4738fad1e86d12dcca2
Tin Aung Khine <tinaungkhine@Tins-MacBook-Pro.local>
2021-04-24 14:33:33 -0400

    u

---
+++ precache-manifest.86350a830064992e627fe42367d7cff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0adb09f57a53b89c7cafda27283a021a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4413b9cb2587519a2dee",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a35ffa22d468f6864709",
+    "url": "/static/js/2.19761168.chunk.js"
+  },
+  {
+    "revision": "2a3a23bf878d7187dc1e20049a44e9ba",
+    "url": "/static/js/2.19761168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4413b9cb2587519a2dee",
+    "url": "/static/js/main.93310325.chunk.js"
+  },
+  {
+    "revision": "e230fa39f3c0a6be624f",
+    "url": "/static/js/runtime-main.9aa0af40.js"
+  }
+]);
